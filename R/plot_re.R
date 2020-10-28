99e5a37e1f26170b6e34e101b239787668b374ba
BenWilliams-NOAA <ben.williams@noaa.gov>
2020-10-27 17:06:21 -0800

    update

--- R/plot_re.R
+++ R/plot_re.R
@@ -24,17 +24,25 @@ plot_re <- function(location){
                        dplyr::mutate(lci = srv_est - srv_est * srv_sd * 1.96,
                                      uci = srv_est + srv_est * srv_sd * 1.96,
                                      yrs = yrs_srv,
-                                     lci = ifelse(lci <0, 0, lci)), by = c("region", "yrs")) %>%
+                                     lci = ifelse(lci <0, 0, lci)),
+                     by = c("region", "yrs")) %>%
+    dplyr::mutate(region = factor(region,
+                                  levels = c("WGOA", "CGOA", "EGOA"))) -> dat
+
+  dat %>%
     ggplot2::ggplot(ggplot2::aes(yrs, biomA / 1000)) +
     ggplot2::geom_ribbon(ggplot2::aes(ymin = LCI / 1000, ymax = UCI / 1000), alpha = 0.15) +
     ggplot2::geom_errorbar(ggplot2::aes(ymin = lci / 1000, ymax = uci / 1000), color = "darkgray") +
     ggplot2::geom_point(ggplot2::aes(y = srv_est / 1000), color = "darkgray") +
     ggplot2::geom_line() +
-    ggplot2::facet_wrap(~region, scales = "free_y", nrow = 3) +
-    ggplot2::ylab("Survey biomass (kt)") +
+    ggplot2::facet_wrap(~region, scales = "free_y", nrow = 3, drop=TRUE) +
+    ggplot2::ylab("Survey biomass (kt)\n") +
     ggplot2::xlab("\nYear") +
-    funcr::theme_report()
+    funcr::theme_report() +
+    ggplot2::scale_x_continuous(breaks = funcr::tickr(dat, year, 5, start = 1980)$breaks,
+                                labels = funcr::tickr(dat, year, 5, start = 1980)$labels)
 
   ggplot2::ggsave(here::here(year, model, "figs", "random_effect.png"),
                   width = 6.5, height = 6.5, units = "in", dpi = 200)
 }
+
